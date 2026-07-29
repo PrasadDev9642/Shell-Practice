67eeab67a5c1b7ece3786a2514e32783915b42e8
PrasadDev9642 <prasadlanka9642@gmail.com>
2026-07-29 18:13:19 +0530

    Practice Shell Scripting code Basics

--- Practice2.sh
+++ Practice2.sh
@@ -8,3 +8,5 @@ echo "$PERSON1:: Hi $PERSON2, How are you?"
 echo "$PERSON2:: Hello $PERSON1, I am fine. How are you?"
 echo "$PERSON1:: What are you learning recently?"
 echo "$PERSON2:: I am learning Shell scripting $PERSON1"
+
+echo "I am Prasad Lanka. I am learning devops"
