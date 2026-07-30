274aa8a73fbf7e9225252d4173eec456237e1a1f
PrasadDev9642 <prasadlanka9642@gmail.com>
2026-07-30 23:24:29 +0530

    coversation

--- 08.Special_Variables.sh
+++ 08.Special_Variables.sh
@@ -3,3 +3,11 @@
 echo "All Variables passed to the Script: $@"
 
 echo "All Variables passed to the Script: $*"
+
+echo "Script Name: $0"
+
+echo "Current Directory: $PWD"
+
+echo "Who is running thsi : $USER"
+
+echo "Home Directory: $Home"
