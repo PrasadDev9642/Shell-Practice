06b3dfa0cac4f8131a5a16f5eed10eef6f5a59f0
PrasadDev9642 <prasadlanka9642@gmail.com>
2026-07-30 20:20:34 +0530

    coversation

--- 02.Coversation.sh
+++ 02.Coversation.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-$PERSON1 = Durga
-$PERSON2 = Prasad
+PERSON1 = Durga
+PERSON2 = Prasad
 
-echo "${PERSON1}:: Hi ${PERSON2},how are you?"
-echo "${PERSON2}:: Hello ${PERSON1},I am fine.how are you?"
-echo "${PERSON1}:: What are you learning recently?"
-echo "${PERSON2}:: i am learning Devsecops recently i joined."
+echo "$PERSON1:: Hi $PERSON2,how are you?"
+echo "$PERSON2:: Hello $PERSON1,I am fine.how are you?"
+echo "$PERSON1:: What are you learning recently?"
+echo "$PERSON2:: i am learning Devsecops recently i joined."
