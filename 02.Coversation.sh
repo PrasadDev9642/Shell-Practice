0d6fc77b741c65d69a25b0864e6c45c866bc7f77
PrasadDev9642 <prasadlanka9642@gmail.com>
2026-07-30 20:16:02 +0530

    coversation

--- 02.Coversation.sh
+++ 02.Coversation.sh
@@ -3,4 +3,7 @@
 $PERSON1 = Durga
 $PERSON2 = Prasad
 
-echo=
+echo "Ramesh:: Hi Suresh,how are you?"
+echo "Suresh:: Hello Ramesh,I am fine.how are you?"
+echo "Ramesh:: What are you learning recently?"
+echo "Suresh:: i am learning Devsecops recently i joined."
